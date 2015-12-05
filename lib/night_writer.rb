c02674a944692922e77e0387bd61b432fb0f7921
Julian Feliciano <julsfelic@gmail.com>
2015-12-04 21:30:45 -0700

    Finish implementation for multiple letters

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,43 +1,80 @@
 require 'pry'
-require_relative 'file_reader'
+require_relative 'file_helper'
 
 class NightWriter
-  attr_accessor :current_word
+  attr_accessor :current_sentence
   attr_reader :braille_chars, :file_helper
 
   def initialize
     @braille_chars = {
       "a" => ["0.", "..", ".."],
-      "b" => ["0.", "..", ".."]
+      "b" => ["0.", "0.", ".."],
+      "c" => ["00", '..', '..'],
+      "d" => ["00", ".0", ".."],
+      "e" => ["0.", ".0", ".."],
+      "f" => ["00", "0.", ".."],
+      "g" => ["00", "00", ".."],
+      "h" => ["0.", "00", ".."],
+      "i" => [".0", "0.", ".."],
+      "j" => [".0", "00", ".."],
+      "k" => ["0.", "..", "0."],
+      "l" => ["0.", "0.", "0."],
+      "m" => ["00", "..", "0."],
+      "n" => ["00", ".0", "0."],
+      "o" => ["0.", ".0", "0."],
+      "p" => ["00", "0.", "0."],
+      "q" => ["00", "00", "0."],
+      "r" => ["0.", "00", "0."],
+      "s" => [".0", "0.", "0."],
+      "t" => [".0", "00", "0."],
+      "u" => ["0.", "..", "00"],
+      "v" => ["0.", "0.", "00"],
+      "w" => [".0", "00", ".0"],
+      "x" => ["00", "..", "00"],
+      "y" => ["00", ".0", "00"],
+      "z" => ["0.", ".0", "00"]
     }
-    @current_word = []
+    @current_sentence = []
     @file_helper = FileHelper.new
   end
 
-  def word
+  def sentence
     file_helper.read
   end
 
-  def convert_word(english_word = word)
-    if english_word.length == 1
-      braille_frag = braille_chars[english_word]
+  def convert_sentence(english_sentence = sentence)
+    if english_sentence.length == 1
+      braille_frag = braille_chars[english_sentence]
       braille_frag.each do |braille|
-        self.current_word << braille
+        self.current_sentence << braille
       end
     else
-      # implement shit
+      first_line  =  []
+      second_line =  []
+      third_line  =  []
+
+      english_sentence.each_char do |char|
+        braille_frag = braille_chars[char]
+        first_line  << braille_frag[0]
+        second_line << braille_frag[1]
+        third_line  << braille_frag[2]
+      end
+
+      current_sentence << [first_line.join]
+      current_sentence << [second_line.join]
+      current_sentence << [third_line.join]
     end
 
-    current_word.join("\n")
+    current_sentence.join("\n")
   end
 
   def return_output
-    file_helper.write(current_word)
+    file_helper.write(current_sentence)
   end
 end
 
 if __FILE__ == $0
   @converter = NightWriter.new
-  @converter.convert_word
+  @converter.convert_sentence
   @converter.return_output
 end
--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -8,7 +8,7 @@ class NightWriterTest < Minitest::Test
 
   def test_takes_in_1_char_and_returns_1_braille
     # We pass in the character we want converted
-    braille_sentence = @converter.convert_word("a")
+    braille_sentence = @converter.convert_sentence("a")
 
     # It should match the correct braille version
     assert_equal braille_sentence, "0.\n..\n.."
@@ -16,9 +16,13 @@ class NightWriterTest < Minitest::Test
 
   def test_takes_in_2_chars_and_returns_braille
     # We pass in the two characters we want converted
-    braille_sentence = @converter.convert_word("ab")
+    braille_sentence = @converter.convert_sentence("ab")
 
     # It should match the correct braille version
-    assert_equal braille_sentence, "0.0.\n..0.\n...."
+    assert_equal braille_sentence, "0.0.\n..0.\n....1"
+  end
+
+  def test_takes_in_a_word_and_returns_braille
+    skip
   end
 end
